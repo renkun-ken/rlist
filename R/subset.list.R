df982c8e191ad2c200b49c2a08807a36c7a885fc
Kun Ren <renkun@outlook.com>
2014-06-05 16:50:21 +0800

    subset.list doc refined

--- R/subset.list.R
+++ R/subset.list.R
@@ -8,12 +8,14 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' x <- list(p1 = list(type="A",score=10),
-#'        p2 = list(type="B",score=8),
-#'        p3 = list(type="B",score=7))
+#' x <- list(p1 = list(type="A",score=list(c1=10,c2=8)),
+#'        p2 = list(type="B",score=list(c1=9,c2=9)),
+#'        p3 = list(type="B",score=list(c1=9,c2=7)))
 #' subset(x,type=="B")
 #' subset(x,select=score)
-#' subset(x,type=="B" & score>=8,paste(type,score,sep=":"))
+#' subset(x,min(score$c1,score$c2)>=8,data.frame(score))
+#' do.call(rbind,
+#'    subset(x,min(score$c1,score$c2)>=8,data.frame(score)))
 #' }
 subset.list <- function(x,subset=TRUE,select=NULL,...) {
   subset <- substitute(subset)
