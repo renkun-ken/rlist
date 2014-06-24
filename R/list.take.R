380701b128fff7f3f24531495f7808d6740034b9
Kun Ren <renkun@outlook.com>
2014-06-25 01:00:08 +0800

    add list.take

---
+++ R/list.take.R
@@ -0,0 +1,16 @@
+#' Take a number of members from a list
+#'
+#' @param x The list from which some members are taken
+#' @param n The number of members to take out
+#' @param force \code{TRUE} disables the length check
+#' @name list.take
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- list(a=1,b=2,c=3)
+#' list.take(x,1)
+#' list.take(x,10)
+#' }
+list.take <- function(x,n,force=FALSE) {
+  x[1:ifelse(force,n,min(length(x),n))]
+}
