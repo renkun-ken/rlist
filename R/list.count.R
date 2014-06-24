014f40e5a3830110861c30988a7deab5ef240917
Kun Ren <renkun@outlook.com>
2014-06-25 00:48:20 +0800

    add list.count

---
+++ R/list.count.R
@@ -0,0 +1,16 @@
+#' Count the number of members that meet given condition
+#'
+#' @param ... Parameters passed to \code{list.filter}
+#' @name list.count
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- list(p1 = list(type="A",score=list(c1=10,c2=8)),
+#'        p2 = list(type="B",score=list(c1=9,c2=9)),
+#'        p3 = list(type="B",score=list(c1=9,c2=7)))
+#' list.count(x,type=="B")
+#' list.count(x,min(unlist(score)) >= 9)
+#' }
+list.count <- function(...) {
+  length(list.filter(...,keep.null=FALSE))
+}
