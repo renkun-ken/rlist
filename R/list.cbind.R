9ee680c692987d21fe8887f5558c87dad38a8796
Kun Ren <renkun@outlook.com>
2014-06-26 00:08:43 +0800

    list.rind, list.cbind

--- R/list.cbind.R
+++ R/list.cbind.R
@@ -1,3 +1,13 @@
+#' Bind all list members by row
+#'
+#' @param x The list
+#' @name list.cbind
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- list(data.frame(i=1:5,x=rnorm(5)),data.frame(y=rnorm(5),z=rnorm(5)))
+#' list.cbind(x)
+#' }
 list.cbind <- function(x) {
   do.call(cbind,x)
 }
--- R/list.rbind.R
+++ R/list.rbind.R
@@ -1,3 +1,15 @@
+#' Bind all list members by row
+#'
+#' @param x The list
+#' @name list.rbind
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- lapply(1:3,function(i) { c(a=i,b=i^2)})
+#' df <- lapply(1:3,function(i) { data.frame(a=i,b=i^2,c=letters[i])})
+#' list.rbind(x)
+#' list.rbind(df)
+#' }
 list.rbind <- function(x) {
   do.call(rbind,x)
 }
