7b975c29b816263a6042b64018bf27ef0b96dbdb
Kun Ren <renkun@outlook.com>
2014-06-26 00:31:53 +0800

    list.serialize

--- R/list.serialize.R
+++ R/list.serialize.R
@@ -1,3 +1,18 @@
+#' Serialize a list
+#'
+#' @param x The list to be serialzied
+#' @param file The file for output
+#' @param type The type of serialization, including native serializer and
+#'    json serializer, which is by default determined by file extension
+#' @param ... Additional parameters passed to the serializer function
+#' @name list.serialize
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- list(a=1,b=2,c=3)
+#' list.serialize(x,"test.dat")
+#' list.serialize(x,"test.json")
+#' }
 list.serialize <- function(x,file,type=tolower(tools::file_ext(file)),...) {
   fun <- paste("list.serialize",type,sep = ".")
   if(existsFunction(fun)) {
