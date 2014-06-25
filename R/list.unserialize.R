e7d07cb6fb6474910ca35dfab2c39120e1cd4d23
Kun Ren <renkun@outlook.com>
2014-06-26 00:32:07 +0800

    list.unserialize

--- R/list.unserialize.R
+++ R/list.unserialize.R
@@ -1,3 +1,16 @@
+#' Unserialize a file
+#'
+#' @param file The file as input
+#' @param type The type of serialization, including native unserializer and
+#'    json unserializer, which is by default determined by file extension
+#' @param ... Additional parameters passed to the unserializer function
+#' @name list.unserialize
+#' @export
+#' @examples
+#' \dontrun{
+#' list.unserialize("test.dat")
+#' list.unserialize("test.json")
+#' }
 list.unserialize <- function(file,type=tolower(tools::file_ext(file)),...) {
   fun <- paste("list.unserialize",type,sep = ".")
   if(existsFunction(fun)) {
