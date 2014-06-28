4787e31907c572543b271dc342294d4a63aa1e45
Kun Ren <renkun@outlook.com>
2014-06-28 13:00:37 +0800

    add list.do

---
+++ R/list.do.R
@@ -0,0 +1,18 @@
+#' Call a function with a list of arguments as provided
+#'
+#' @param x The list
+#' @param fun The function to call
+#' @param ... The additional parameters passed to \code{do.call}
+#' @name list.do
+#' @export
+#' @examples
+#' \dontrun{
+#' x <- lapply(1:3,function(i) { c(a=i,b=i^2)})
+#' df <- lapply(1:3,function(i) { data.frame(a=i,b=i^2,c=letters[i])})
+#' list.do(x,rbind)
+#' list.do(x,rbind)
+#' as.list(1:10) %>>% list.map(x -> list.do(x,rnorm))
+#' }
+list.do <- function(x,fun,...) {
+  do.call(fun,as.list(x),...)
+}
