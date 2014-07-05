0b6c060df5617cb45589028d4d8e80c920ad65de
Kun Ren <renkun@outlook.com>
2014-07-05 18:01:18 +0800

    list.cases supports list-like cases

--- R/list.cases.R
+++ R/list.cases.R
@@ -2,7 +2,11 @@
 #'
 #' @param .data A list
 #' @param expr The expression to evaluate to find cases
-#' @param sort Should the cases be sorted in ascending order?
+#' @param ... Additional parameters passed to \code{unique}
+#' @param simplify \code{logical}. Should the results be simplified? Only works
+#'    when each member in the result is a vector of length 1.
+#' @param sort \code{logical}. Should the cases be sorted in ascending order?
+#'    Ignored when the result is atomic.
 #' @name list.cases
 #' @export
 #' @examples
@@ -13,8 +17,12 @@
 #' list.cases(x,type)
 #' list.cases(x,mean(unlist(score)))
 #' }
-list.cases <- function(.data,expr,sort=TRUE) {
-  cases <- unique(unlist(list.map.internal(.data,substitute(expr)),
-    use.names = FALSE))
-  if(sort) sort(cases) else cases
+list.cases <- function(.data,expr,...,simplify=TRUE,sort=TRUE) {
+  values <- list.map.internal(.data,substitute(expr))
+  cases <- unique(values,...)
+  if(simplify) {
+    lens <- vapply(cases,length,integer(1L),USE.NAMES = FALSE)
+    if(all(lens <= 1L)) cases <- unlist(cases,use.names = FALSE)
+  }
+  if(sort && is.atomic(cases)) sort(cases) else cases
 }
