e45e3e125c5d66972995db328c7ec91bf18f024a
Kun Ren <renkun@outlook.com>
2014-07-07 00:16:01 +0800

    fix typo for list.cbind

--- R/list.cbind.R
+++ R/list.cbind.R
@@ -1,11 +1,12 @@
-#' Bind all list members by row
+#' Bind all list members by column
 #'
 #' @param .data \code{list}
 #' @name list.cbind
 #' @export
 #' @examples
 #' \dontrun{
-#' x <- list(data.frame(i=1:5,x=rnorm(5)),data.frame(y=rnorm(5),z=rnorm(5)))
+#' x <- list(data.frame(i=1:5,x=rnorm(5)),
+#'    data.frame(y=rnorm(5),z=rnorm(5)))
 #' list.cbind(x)
 #' }
 list.cbind <- function(.data) {
