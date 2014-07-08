cc1ce609292a42610d6c098b64ca8f1804151d78
Kun Ren <renkun@outlook.com>
2014-07-09 00:59:49 +0800

    functions refined

--- R/functions.R
+++ R/functions.R
@@ -5,22 +5,19 @@ lambda <- function(expr) {
   .lambda$expr <- expr
   if(is.call(expr) && length(expr[[1L]])==1L) {
     symbol <- as.character(expr[[1L]])
-    len <- length(expr)
-    if(symbol == "~") {
-      if(len != 3L) stop("Invalid lambda expression")
-      symbols <- expr[[2L]]
-      .lambda$expr <- expr[[3L]]
-    } else if(symbol == "<-") {
-      if(len != 3L) stop("Invalid lambda expression")
+    if(symbol == "<-") {
       symbols <- expr[[3L]]
       .lambda$expr <- expr[[2L]]
+    } else if(symbol == "~") {
+      symbols <- expr[[2L]]
+      .lambda$expr <- expr[[3L]]
     } else {
       return(.lambda)
     }
     if(is.name(symbols)) {
       .lambda$symbols[1L] <- as.character(symbols)
     } else if(is.call(symbols)) {
-      symbols <- as.character(as.list(symbols)[-1])
+      symbols <- as.character(as.list(symbols)[-1L])
       indices <- which(symbols != "")
       .lambda$symbols[indices] <- symbols[indices]
     } else {
