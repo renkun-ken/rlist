a0d84f4ee78a55065aaec6f95ec7b87068622b4b
Kun Ren <renkun@outlook.com>
2014-07-08 19:15:18 +0800

    refined docs for list.stack

--- R/list.stack.R
+++ R/list.stack.R
@@ -1,11 +1,18 @@
 #' Stack all list members to construct a \code{data.frame}
 #'
-#' @param .data \code{list}
+#' @param .data \code{list} of \code{vector}s, \code{list}s,
+#'    or \code{data.frame}s.
 #' @name list.stack
 #' @export
 #' @examples
 #' \dontrun{
-#' x <- lapply(1:3,function(i) { list(a=i,b=i^2,c=letters[i])})
+#' x <- lapply(1:3, function(i) { list(a=i,b=i^2) })
+#' list.stack(x)
+#'
+#' x <- lapply(1:3, function(i) { list(a=i,b=i^2,c=letters[i])})
+#' list.stack(x)
+#'
+#' x <- lapply(1:3, function(i) { data.frame(a=i,b=i^2,c=letters[i]) })
 #' list.stack(x)
 #' }
 list.stack <- function(.data) {
