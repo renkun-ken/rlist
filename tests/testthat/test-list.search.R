d0d6802c3ce56aa8c2f62e1e64ff1d24f2061a2e
Kun Ren <renkun@outlook.com>
2014-07-30 10:54:32 +0800

    update info

---
+++ tests/testthat/test-list.search.R
@@ -0,0 +1,10 @@
+context("list.search")
+
+test_that("list.search", {
+
+  # simple list
+  x <- list(p1 = list(type="A",score=list(c1=10,c2=8)),
+    p2 = list(type="B",score=list(c1=9,c2=9)),
+    p3 = list(type="B",score=list(c1=9,c2=7)))
+
+})
