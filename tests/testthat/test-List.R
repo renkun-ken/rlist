17ce873ad99f62258fe0dc2a33258bf73ee8df5e
Kun Ren <renkun@outlook.com>
2014-08-04 19:40:02 +0800

    add more test cases for List

--- tests/testthat/test-List.R
+++ tests/testthat/test-List.R
@@ -32,4 +32,18 @@ test_that("List", {
   expect_identical(List(x)$common(names(score))[], c("c1","c2"))
   expect_identical(List(x)$count(type == "B")[], 2L)
   expect_identical(List(x)$filter(type == "B")[], list.filter(x,type=="B"))
+  expect_identical(List(1:10)$find(. %% 2 == 0,3)[],c(2L,4L,6L))
+  expect_identical(List(1:10)$findi(. %% 2 == 0,3)[],c(2L,4L,6L))
+  expect_identical(List(1:10)$group(. %% 3)[],list.group(1:10,. %% 3))
+  expect_identical(List(list(1:10,2:15))$search(any(. >= 11))[],
+    list.search(list(1:10,2:15),any(. >= 11)))
+  expect_identical(local({
+    i <- 12
+    List(list(1:10,2:15))$search(any(. >= i))[]
+  }),list.search(list(1:10,2:15),any(. >= 12)))
+  expect_identical(List(1:200)$table(. %% 2, . %% 3)[],
+    list.table(1:200, . %% 2, . %% 3))
+  expect_identical(List(x)$update(mean=mean(unlist(score)))[],
+    list.update(x,mean=mean(unlist(score))))
+
 })
