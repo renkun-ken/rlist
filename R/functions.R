fa71ff154f330295c2685aad6cc98de901a9dbe8
Kun Ren <ken@renkun.me>
2014-08-13 23:40:00 +0800

    refine lambda

--- R/functions.R
+++ R/functions.R
@@ -1,29 +1,3 @@
-.lambda <- list(symbols=c(".",".i",".name"))
-.nsymbol <- length(.lambda$symbols)
-.nfsymbol <- 2L + .nsymbol
-
-lambda <- function(expr) {
-  .lambda$expr <- expr
-  if(is.call(expr) && length(expr[[1L]])==1L) {
-    symbol <- as.character(expr[[1L]])
-    if(symbol == "<-") {
-      symbols <- expr[[3L]]
-      .lambda$expr <- expr[[2L]]
-    } else if(symbol == "~") {
-      symbols <- expr[[2L]]
-      .lambda$expr <- expr[[3L]]
-    } else return(.lambda)
-    if(is.name(symbols))
-      .lambda$symbols[1L] <- as.character(symbols)
-    else if(is.call(symbols)) {
-      symbols <- as.character(symbols)[-1L]
-      indices <- which(symbols != "")
-      .lambda$symbols[indices] <- symbols[indices]
-    } else stop("Invalid lambda expression")
-  }
-  .lambda
-}
-
 dots <- function(...) {
   eval(substitute(alist(...)))
 }
@@ -32,9 +6,12 @@ list.sort.functions <- list(desc=`-`)
 
 setnames <- `names<-`
 setclass <- `class<-`
+setmembers <- `[<-`
 
 getnames <- function(x, null = NULL)
   if(is.null(names(x))) null else names(x)
+
+is.empty <- function(x) length(x) == 0L
 
 set_argnames <- function(args,data=args) {
   argnames <- names(args)
@@ -50,7 +27,3 @@ set_argnames <- function(args,data=args) {
   else if(is.vector(x) && !is.null(names(x))) setclass(x,"list")
   else NULL
 }
-
-setmembers <- `[<-`
-
-is.empty <- function(x) length(x) == 0L
