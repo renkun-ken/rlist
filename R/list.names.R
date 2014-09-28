772e8f34d2089e9441f102c918be6d45863f0172
Kun Ren <ken@renkun.me>
2014-09-28 18:18:19 +0800

    add examples to list.names

--- R/list.names.R
+++ R/list.names.R
@@ -4,6 +4,11 @@
 #' for each list element. If missing then the names of the list will be
 #' returned
 #' @export
+#' @examples
+#' list.names(c(1,2,3))
+#' list.names(c(a=1,b=2,c=3))
+#' list.names(c(1,2,3),letters[.])
+#' list.names(list(list(name="A",value=10),list(name="B",value=20)), name)
 list.names <- function(.data, expr) {
   if(missing(expr)) return(names(.data))
   values <- list.map.internal(.data,substitute(expr),envir = parent.frame())
