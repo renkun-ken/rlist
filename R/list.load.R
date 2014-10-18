6d745a8fe98ff0ff72a6cdbd2fcc1720c3a9fc87
Kun Ren <ken@renkun.me>
2014-10-18 17:51:09 +0800

    list.load supports multi-loading (close #74)

--- R/list.load.R
+++ R/list.load.R
@@ -12,17 +12,19 @@
 #' list.load("list.yaml")
 #' list.load("list.json")
 #' }
-list.load <- function(file,type=tolower(tools::file_ext(file)),...) {
-  fun <- paste("list.load",type,sep = ".")
-  if(existsFunction(fun)) {
-    fun <- get(fun,mode = "function")
-    fun(file,...)
-  } else {
-    list.load.rdata(file,...)
-  }
+list.load <- function(file, type = tolower(tools::file_ext(file)), ...) {
+  fun <- paste("list.load", type, sep = ".")
+  map(function(file, fun, ...) {
+    x <- if(existsFunction(fun)) {
+      fun <- get(fun, mode = "function")
+      fun(file, ...)
+    } else {
+      list.load.rdata(file, ...)
+    }
+  }, file, fun, MoreArgs = list(...))
 }
 
-list.load.json <- function(file,...) {
+list.load.json <- function(file, ...) {
   jsonlite::fromJSON(file,
     simplifyDataFrame = FALSE,
     ...)
