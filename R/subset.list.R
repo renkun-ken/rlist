5a4f0b716b4c010b8439540c009bc1b164de3ed8
Kun Ren <renkun@outlook.com>
2015-02-17 16:44:49 +0800

    refine list subsetting

--- R/subset.list.R
+++ R/subset.list.R
@@ -1,8 +1,6 @@
-. <- NULL
-
-#' Return subsets of a list which meet conditions.
+#' Subset a list by a logical condition
 #'
-#' @param x The list to be subsetted
+#' @param x The list to subset
 #' @param subset A logical lambda expression of subsetting condition
 #' @param select A lambda expression to evaluate for each selected item
 #' @param ... Additional parameters
@@ -11,14 +9,17 @@
 #' x <- list(p1 = list(type='A',score=list(c1=10,c2=8)),
 #'        p2 = list(type='B',score=list(c1=9,c2=9)),
 #'        p3 = list(type='B',score=list(c1=9,c2=7)))
-#' subset(x, type=='B')
-#' subset(x, select=score)
-#' subset(x, min(score$c1,score$c2) >= 8, data.frame(score))
+#' subset(x, type == 'B')
+#' subset(x, select = score)
+#' subset(x, min(score$c1, score$c2) >= 8, data.frame(score))
+#' subset(x, type == 'B', score$c1)
 #' do.call(rbind,
-#'    subset(x,min(score$c1,score$c2) >= 8,data.frame(score)))
-subset.list <- function(x, subset = TRUE, select = ., ...) {
+#'    subset(x, min(score$c1, score$c2) >= 8, data.frame(score)))
+subset.list <- function(x, subset, select, ...) {
   envir <- parent.frame()
-  subset.items <- x[list.is.internal(x, substitute(subset), envir)]
-  select.items <- list.map.internal(subset.items, substitute(select), envir)
+  subset.items <- if(missing(subset)) x else
+    x[list.is.internal(x, substitute(subset), envir)]
+  select.items <- if(missing(select)) subset.items else
+    list.map.internal(subset.items, substitute(select), envir)
   list.clean(select.items)
-} 
+}
