dd6d0cfa4d5d19a78ce6b999e5370504b8afd42d
Kun Ren <renkun@outlook.com>
2015-07-31 11:58:11 +0800

    attribute credit to Tommy (http://stackoverflow.com/users/662787/tommy) for the implementation of list.flatten at http://stackoverflow.com/a/8139959/2906900

--- R/list.flatten.R
+++ R/list.flatten.R
@@ -1,7 +1,12 @@
 #' Flatten a nested list to a one-level list
 #'
+#' @details
+#' The function is essentially a slightly modified version of \code{flatten2}
+#' provided by Tommy at \link{http://stackoverflow.com/a/8139959/2906900} who
+#' has full credit of the implementation of this function.
 #' @param x \code{list}
 #' @param use.names \code{logical}. Should the names of \code{x} be kept?
+#' @author Tommy (\link{http://stackoverflow.com/users/662787/tommy})
 #' @export
 #' @examples
 #' p <- list(a=1,b=list(b1=2,b2=3),c=list(c1=list(c11='a',c12='x'),c2=3))
