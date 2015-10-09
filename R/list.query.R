2358c60912724947ab786f3390b8f6fc7e514bdd
Kun Ren <renkun@outlook.com>
2015-10-09 09:54:48 +0800

    support query a json file at local or http remote

--- R/list.query.R
+++ R/list.query.R
@@ -9,17 +9,33 @@
 #'
 #' The function might be less performant with large list or frequent
 #' calling.
-#' @param x a list
-#' @param query a JsonPath string
-#' @param with a list of elements to be used in \code{query}
+#' @param x a list, JSON string, file path or a web link to a json document.
+#' @param query a JsonPath string.
+#' @param with a list of elements to be used in \code{query}.
+#' @param validate \code{TRUE} to validate the JSON document if \code{x}
+#' is supplied one. Ignored if \code{x} is a list.
+#' @param ... Additional parameters. If \code{x} is supplied a file path
+#' or a web address, then these arguments will be passed to \code{readLines}
+#' that reads the file content.
 #' @importFrom V8 new_context
+#' @importFrom jsonlite validate
 #' @seealso \url{https://github.com/s3u/JSONPath}
 #' @export
-list.query <- function(x, query, with = list()) {
+list.query <- function(x, query, with = list(), validate = TRUE, ...) {
   js_context <- V8::new_context(global = "window")
+  if (is.empty(x)) return(x)
   if (is.list(x)) {
     js_context$assign("data", x, auto_unbox = TRUE)
   } else if (is.character(x)) {
+    if (length(x) > 1L) {
+      warning("Only the first value of x is taken as input")
+      x <- x[1]
+    }
+    if (file.exists(x) || grepl("^http", x)) x <- paste(readLines(x, warn = FALSE, ...), collapse = "\n")
+    if (validate) {
+      val <- jsonlite::validate(x)
+      if (!val) stop("Invalid JSON input:", attr(val, "err", TRUE))
+    }
     js_context$eval(sprintf("var data = %s;", x))
   } else {
     stop("x must be a list or json string")
