83eed36c49e5975fa0c77f4b560b256c58fd5b23
Kun Ren <renkun@outlook.com>
2015-10-09 10:43:21 +0800

    list.table will directly call table upon data if ... is missing

--- R/list.table.R
+++ R/list.table.R
@@ -1,6 +1,8 @@
 #' Generate a table for a list by expression
 #' @param .data A \code{list} or \code{vector}
-#' @param ... A group of lambda expressions
+#' @param ... A group of lambda expressions. If missing,
+#' \code{table} will be directly called upon \code{.data} with
+#' \code{table.args}.
 #' @param table.args \code{list}. The additional parameters
 #'    passed to \code{table}
 #' @export
@@ -12,12 +14,13 @@
 #' list.table(x, type, c1 = score$c1)
 #' list.table(x, type, score$c1, table.args = list(dnn=c('type','c1')))
 list.table <- function(.data, ..., table.args = list(useNA = "ifany")) {
-  args <- set_argnames(dots(...))
+  if (missing(...)) return(do.call(table, c(list(.data), table.args)))
+  args <-  set_argnames(dots(...))
   envir <- parent.frame()
   items <- lapply(args, function(arg) {
     values <- list.map.internal(.data, arg, envir)
     values[vapply(values, is.null, logical(1L))] <- NA
     c(values, recursive = TRUE)
   })
-  do.call("table", c(items, table.args))
-} 
+  do.call(table, c(items, table.args))
+}
